8349027f0acdf5e1042ca93ccf5d9a9fbe7e1b05
Nicolás Venturo <nicolas.venturo@gmail.com>
2015-10-05 16:24:33 -0300

    Triangles are nicer.

--- javascripts/dottastico.js
+++ javascripts/dottastico.js
@@ -8,10 +8,10 @@ function set_dots(){
 	var dottastico_height = 0;
 	$('.dottable_section').each(function(index){
 		var offsetted_index = index + 1;
-		var shape = $('.dottastico').data("shape") || "circle";
+		var shape = $('.dottastico').data("shape") || "triangle";
 		var position = $('.dottastico').data("position") || "left";
 		$('.dottastico').addClass("dottastico-"+position);
-		$('.dottastico').append("<a href='javascript:void(0)' class='dot-link'><div class='dot "+shape+"' data-index="+offsetted_index+"></div></a>");	
+		$('.dottastico').append("<a href='javascript:void(0)' class='dot-link'><div class='dot "+shape+"' data-index="+offsetted_index+"></div></a>");
 		dottastico_height = dottastico_height + (parseInt($('.dottastico').data("size").substring(0, $('.dottastico').data("size").length - 2))) + 15;
 	});
 	$('.dottastico').css({
@@ -40,7 +40,7 @@ function change_active_section(sections){
 
 function change_active_section_class(section){
 	$('.dot').each(function(){
-		$(this).removeClass("active-dot");	
+		$(this).removeClass("active-dot");
 	});
 	$(section).addClass("active-dot");
 }
